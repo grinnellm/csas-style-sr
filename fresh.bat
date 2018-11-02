87503dce0d70d3cda9cafceaa2e982e7380fad4b
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2018-11-02 08:53:10 -0700

    Clean up french versions too

--- fresh.bat
+++ fresh.bat
@@ -1,10 +1,10 @@
 # Clean up the directory after a LaTeX build. Windows version
-del example.pdf
-del example.log
-del example.aux
-del example.bbl
-del example.blg
-del example.tex
-del example.toc
-del example.upa
+del example*.pdf
+del example*.log
+del example*.aux
+del example*.bbl
+del example*.blg
+del example*.tex
+del example*.toc
+del example*.upa
 rmdir /S /Q figure
